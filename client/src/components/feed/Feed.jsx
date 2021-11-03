f451b35f40b7e6cc8082da03f2b85d7e0dae3a7f
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-03 23:26:34 +0530

    Update Feed.jsx

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -16,7 +16,7 @@ function Feed({ username }) {
               ? await axios.get('/posts/profile/' + username)   // for profile page
               : await axios.get('/posts/timeline/' + user._id);  // for home page
             setPosts(res.data.sort((p1, p2)=>{
-                return new Date(p2.createdAt) - new Date(p1.createdAt);
+                return new Date(p2.createdAt) - new Date(p1.createdAt);  // to sort the posts
             }));
         }
         fetchPosts();

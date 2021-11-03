27c3d978b59bcaddb56db8cd07e662c4d702e63b
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-04 00:31:13 +0530

    Add files via upload

---
+++ server/routes/posts.js
@@ -0,0 +1,115 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+
+
+// Create a Post
+
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update a Post
+
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json({ message: "Post updated successfully" });
+        }else {
+            res.status(403).json({ message: "You can update only your posts" });
+        }
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete a Post
+
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+          await post.deleteOne();
+          res.status(200).json("the post has been deleted");
+      }else {
+          res.status(403).json("you can delete only your post");
+      }
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Like / Unlike a Post
+
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(req.params.id !== req.body.userId){
+            if (!post.likes.includes(req.body.userId)) {
+                await post.updateOne({ $push: { likes: req.body.userId } });
+                res.status(200).json({ message: "Post Liked" });
+            }else {
+                await post.updateOne({ $pull: { likes: req.body.userId } });
+                res.status(200).json({ message: "Post Unliked" });
+            }
+        }
+        else{
+            res.status(403).json({ message: "You Can't like your Posts"});
+        }
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get a Post
+
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get Timeline Posts
+
+router.get("/timeline/:userId", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.params.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPosts));
+    }catch(err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
+
+// Get User's All Posts
+
+router.get("/profile/:username", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username });  // finding the user
+        const posts = await Post.find({ userId: user._id });   // finding posts with userId
+        res.status(200).json(posts);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;

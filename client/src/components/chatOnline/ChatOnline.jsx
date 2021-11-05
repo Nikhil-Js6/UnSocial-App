39c1e56f94ead906c0a21d5d8d10e3e4983fae06
Nikhil <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-11-05 21:33:50 +0530

    Add files via upload

---
+++ client/src/components/chatOnline/ChatOnline.jsx
@@ -0,0 +1,36 @@
+import './chatOnline.css';
+
+export default function ChatOnline() {
+    return (
+        <div className='chatOnline'>
+            <div className='chatOnlineFriend'>
+                <div className='onlineImgWrapper'>
+                    <img className='chatOnlineUserImg' src="https://wallpaperaccess.com/full/945376.jpg" alt=""/>
+                    <span className='chatOnilneBadge'></span>
+                </div>
+                <span className='chatOnlineUsername'>Baby</span>
+            </div>
+            <div className='chatOnlineFriend'>
+                <div className='onlineImgWrapper'>
+                    <img className='chatOnlineUserImg' src="https://wallpaperaccess.com/full/945376.jpg" alt=""/>
+                    <span className='chatOnilneBadge'></span>
+                </div>
+                <span className='chatOnlineUsername'>Baby</span>
+            </div>
+            <div className='chatOnlineFriend'>
+                <div className='onlineImgWrapper'>
+                    <img className='chatOnlineUserImg' src="https://wallpaperaccess.com/full/945376.jpg" alt=""/>
+                    <span className='chatOnilneBadge'></span>
+                </div>
+                <span className='chatOnlineUsername'>Baby</span>
+            </div>
+            <div className='chatOnlineFriend'>
+                <div className='onlineImgWrapper'>
+                    <img className='chatOnlineUserImg' src="https://wallpaperaccess.com/full/945376.jpg" alt=""/>
+                    <span className='chatOnilneBadge'></span>
+                </div>
+                <span className='chatOnlineUsername'>Baby</span>
+            </div>
+        </div>
+    )
+}
